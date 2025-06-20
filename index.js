fec7a61dc7d6c19710d82712d38de8c800341fda
SaurabhSawade <saurabhsawade6@gmail.com>
2025-06-20 07:07:39 +0530

    refactor: update MongoDB connection to use new options

--- index.js
+++ index.js
@@ -10,9 +10,17 @@ const app = express();
 app.use(cors({ origin: 'https://node-jwt-frontend.vercel.app', credentials: true }));
 app.use(express.json());
 
-mongoose.connect(process.env.MONGO_URI)
-  .then(() => console.log('MongoDB connected'))
-  .catch(err => console.error(err));
+// mongoose.connect(process.env.MONGO_URI)
+//   .then(() => console.log('MongoDB connected'))
+//   .catch(err => console.error(err));
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+}).then(() => {
+  console.log('✅ MongoDB connected');
+}).catch((err) => {
+  console.error('❌ MongoDB connection error:', err);
+});
 
 app.use('/api', authRoutes);
 
